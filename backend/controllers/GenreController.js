16623d7630a334bfb414f165f7e66ee22f5d9279
twei-wsl <tw286work@gmail.com>
2023-03-06 16:47:28 +1100

    added some genre controller

--- backend/controllers/GenreController.js
+++ backend/controllers/GenreController.js
@@ -1,12 +1,49 @@
 const Director = require("../models/Genre")
 const async = require("async")
+const Genre = require("../models/Genre")
+const Movie = require("../models/Movie")
 
 exports.genre_list = (req, res, next) => {
-  res.json({message: "genre list"})
+  Genre.find()
+      .sort("name")
+      .exec()
+      .then(results => {
+        if(results === null){
+          const err = new Error("No genres")
+          err.status = 404
+          return next(err)
+        }
+        res.json(results)
+      }).catch(err => next(err))
 }
 
 exports.genre_detail = (req, res, next) =>{ 
-  res.json({message: "genre detail", genre: req.params.id})
+  async.parallel(
+    {
+      movies: (callback) => {
+        Movie.find({genre: req.params.id})
+              .populate("director")
+              .exec(callback)
+      },
+      genre: (callback) => {
+        Genre.findById(req.params.id).exec(callback)
+      }
+    },
+    (err, results) => {
+      if(err) next(err)
+      if(results.genre === null){
+        const err = new Error("Genre not found")
+        err.status = 404
+        return next(err)
+      }
+      res.json({
+        title: "Genre Detail",
+        genre: results.genre,
+        movies: results.movies
+      })
+    }
+  )
+
 }
 
 exports.genre_create_get = (req, res, next) => {
